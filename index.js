972f500e522dfdaa4e5a8b45dde8602dbd65491a
Voxlinou <71925280+voxlinou1@users.noreply.github.com>
2022-04-24 01:51:12 +0000

    Typescript support
    Added typescript notation for people who code on typescript

--- index.js
+++ index.js
@@ -1,102 +1,64 @@
-const Discord = require('discord.js');
+const {
+    Client,
+    CommandInteraction,
+    ButtonInteraction,
+    Interaction,
+    InteractionCollector,
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
+    MessageOptions,
+    UserResolvable,
+} = require('discord.js');
 
-module.exports.PageSystem = class PageSystem {
-    /**
-     * @param {Discord.Client} client Your client.
-     */
+export class PageSystem {
     constructor(client) {
-        this.client = client || null;
-        /**
-         * The index of the current showed embed.
-         */
+        this.client = client;
         this.index = 0;
-        /**
-         * The id that is allowed to use the embed buttons
-         */
         this.id = null;
-        /**
-         * The embeds
-         */
         this.embeds = [];
-        /**
-         * "interaction" "message" The type of reply (interaction/message)
-         */
-        this.ctx = null;
-        /**
-         * The command message that triggered the embed page system
-         */
         this.message = null;
-        /**
-         * The reply interaction (if the command was an interaction)
-         */
         this.interaction = null;
-        /**
-         * The collector of interactions (if the message got sent)
-         */
         this.collector = null;
-        /**
-         * If the collector ended
-         */
         this.ended = false;
-        /**
-         * If the reply got deleted.
-         */
+        this.started = false;
         this.deleted = false;
-        /**
-         *The duration of the collector
-         */
         this.duration = 1000 * 60;
-        /**
-         * The emojis to use as buttons
-         */
         this.emojis = {
             previous: "⬅️",
             stop: "⏹️",
             next: "➡️"
         };
-        /**
-         * The only component row containing the buttons
-         */
-        this._row = [
-            ['previous', this.emojis.previous, 'PRIMARY'],
-            ['stop', this.emojis.stop, 'DANGER'],
-            ['next', this.emojis.next, 'PRIMARY']
+        this.footer = (index, total) => total - 1 ? `📜 Page ${index}/${total}` : '';
+        this.filter = (i) => this.id === i.user.id;
+    }
+    get ctx() {
+        return this.message ? "MESSAGE" : this.interaction ? "INTERACTION" : null;
+    }
+    get _row() {
+        return [
+            ['previous', 'PRIMARY'],
+            ['stop', 'DANGER'],
+            ['next', 'PRIMARY']
         ].map(arr => {
             return {
                 name: arr[0],
-                emoji: arr[1],
-                style: arr[2]
+                emoji: this.emojis[arr[0]],
+                style: arr[1]
             }
-        })
-        /**
-         * @param {Number} i Current embed page
-         * @param {Number} l Embed page length
-         * @returns {string}
-         */
-        this.footer = (i, l) => l - 1 ? `📜 Page ${i}/${l}` : '';
-        /**
-         * Checks if interaction can interact.
-         * @param {Discord.ButtonInteraction}
-         * @returns {Boolean}
-         */
-        this.filter = i => this.id === i.user.id;
-        this.e = (msg) => { throw new Error(msg); };
+        });
+    }
+    get type() {
+        return this.ctx.toLowerCase();
     }
 
-    /**
-     * @description How much time untill the collector has finished.
-     * @returns {Number} Time in milliseconds
-    */
     get endUntill() {
         return this.collector ? (Date.now() - this.collector.options.time) : null;
     }
-    /**
-     * @param {Boolean} disabled If all of the buttons should be disabled.
-     * @returns {Discord.MessageActionRow} The row.
-     * @description Gives all rows
-     */
+
     getRow(disabled = false) {
-        let ar = new Discord.MessageActionRow()
+        let ar = new MessageActionRow()
         let previous = this.embeds[this.index - 1];
         let next = this.embeds[this.index + 1];
         function format(a) {
@@ -105,7 +67,7 @@ module.exports.PageSystem = class PageSystem {
                 || (a.name == "next" && !next)
                 || false;
 
-            let button = new Discord.MessageButton()
+            let button = new MessageButton()
                 .setCustomId(a.name)
                 .setEmoji(a.emoji)
                 .setStyle(a.style)
@@ -116,242 +78,174 @@ module.exports.PageSystem = class PageSystem {
 
         return ar.components.length ? ar : null;
     }
-    /**
-     * @param {string | Discord.GuildMember | Discord.User} user The ID of a new person that will be able to interact with the buttons.
-     * @returns {this}
-     * @example 
-     * // For messages :
-     * .addUser(message.author.id)
-     * // For interactions :
-     * .addUser(interaction.user.id)
-     */
+
     setUserID(user) {
-        if (!user) throw this.e('No ID given.')
-        if (user instanceof Discord.GuildMember || user instanceof Discord.User)
-            user = user.id;
-        if (typeof (user) !== "string") return this.e(`"${id}" is not a string (ID).`)
-        if (!Number(user)) return this.e(`"${id}" is not a valid ID.`)
-        this.id = user;
-        return this;
-    }
-    /**
-     * @param {Function} func The function to (when given the page index) should return the footer text.
-     * @returns {this}
-     * @example
-     * .setFooter((i,l) => `Page ${i} of ${l}`)
-     */
-    setFooter(func = () => '') {
-        if (typeof (func) !== "function") return this.e(`${func} is not a function but a ${typeof (func)}.`)
-        this.footer = func;
-        return this;
-    }
-    /**
-     * @param {Discord.MessageEmbed[]} embeds An array of the embeds to use.
-     * @returns {this}
-     * @example
-     * .setEmbeds([
-     *     new Discord.MessageEmbed().setDescription('First embed')
-     *     new Discord.MessageEmbed().setDescription('Middle embed')
-     *     new Discord.MessageEmbed().setDescription('Last embed')
-     * ])
-     */
-    setEmbeds(embeds) {
-        if (!embeds) return this.e('No embed given')
-        if (embeds instanceof Discord.MessageEmbed) embeds = [embeds];
-        if (!Array.isArray(embeds)) return this.e(`"${embeds}" is not an array but a ${typeof(embeds)}.`)
-        if (!embeds.length) return this.e(`Embed pages is an empty array.`)
-        const notAnEmbed = embeds.find(e => typeof(e) !== "object");
-        if (notAnEmbed) return this.e(`Array contains a none-embed at index ${embeds.indexOf(notAnEmbed)} (${notAnEmbed})`)
-        this.embeds = embeds;
-        this.fixEmbeds();
+        this.id = this.client.users.resolve(user).id;
         return this;
     }
 
-    /**
-     * @param {Discord.MessageEmbed} embed 
-     * @returns {this}
-     */
-    addEmbed(embed) {
-        if (!embed) return this.e('No embed given')
-        if ((embed instanceof Discord.MessageEmbed)) return this.e(`"${embed}" is not an embed.`)
-        this.setEmbeds([...this.embeds,embed]);
+    setFooter(func) {
+        this.footer = func || (() => '');
         return this;
     }
-    /**
-     * @param {Discord.MessageEmbed[][0]} embed 
-     * @returns {this}
-     */
-    addEmbeds(...embeds) {
-        if (!embeds) return this.e('No embeds given')
-        embeds = Array.isArray(embeds[0]) ? embeds[0] : embeds;
+
+    setEmbeds(embeds) {
+        embeds = this.fixEmbeds(embeds);
+        this.embeds = embeds;
+        return this;
+    }
+
+    addEmbed(embed) {
+        return this.addEmbeds([embed]);
+    }
+    fixEmbeds(embeds) {
+        embeds = embeds instanceof MessageEmbed ? [embeds] : embeds;
+        embeds = Array.isArray(embeds[0]) ? embeds[0] : embeds
+        return embeds;
+    }
+
+    addEmbeds(embeds) {
+        embeds = this.fixEmbeds(embeds);
         embeds.forEach(embed => this.addEmbed(embed));
         return this;
     }
-    /**
-     * @param {Number} duration How long should the embed pages work for (milliseconds)
-     * @returns {this}
-     * @example // If you want the pages to last 1 minute
-     * .setDuration(1000 * 60) // 1000ms = 1s, so 1s * 60 = 1m.
-     * // If you want it to last 5 minutes :
-     * .setDuration(100 * 60 * 5) // Same than before, 1m * 5 = 5m.
-     * // You can even use 1e5 syntax:
-     * .setDuration(1e5) // 100000ms (100s)
-     * .setDuration(100000) // 100000ms (100s)
-    */
-    setTime(duration) {
-        if (!duration) return this.e('No duration given.')
-        if (typeof (duration) !== "number") return this.e(`${duration} is not a number.`)
-        if (isNaN(duration)) return this.e(`Duration is NaN.`);
-        if (duration < 0) return this.e(`${duration} can't be negative.`);
+
+    setDuration(duration) {
         this.duration = duration
         return this;
     }
-    _handleEdit() {
-        if (!this.ctx) return this.e('Embed not sent yet.')
-        if (this.ended) return this.e('Page system has already ended.')
-        return true;
+    addDuration(duration) {
+        this.duration += duration
+        return this;
     }
+
     next() {
-        this._handleEdit();
-        return this.setIndex(this.index + 1)
+        if (this.canEdit()) return this.setIndex(this.index + 1)
+        else return Promise.resolve();
     }
 
     previous() {
-        this._handleEdit();
-        return this.setIndex(this.index - 1)
+        if (this.canEdit()) return this.setIndex(this.index - 1)
+        else return Promise.resolve();
     }
-    /**
-     * @param {Number} index The index of the page you want to select 
-     * @returns {this}
-     * @example // For the first page, use
-     * ps.setIndex(0)
-     * // For the second,
-     * ps.setIndex(1)
-     * // For the last page, use
-     * ps.setIndex(this.index - 1)
-     */
+
     setIndex(index) {
-        if (!this.ctx) return this.e('Embed not sent yet.')
-        if (this.ended) return this.e('Page system has already ended.')
-        if (typeof (index) !== "number") return this.e(`'${index}' is not a number.`)
-        if (isNaN(index)) return this.e(`'${index}' is NaN.`)
-        if (!this.embeds[index]) return this.e(`No page at index '${index}'`)
         this.index = index;
+        if (this.canEdit()) this.update()
         return this;
     }
-    /**
-     * @description Replaces every embed's footer by the wanted footer.
-     */
-    fixEmbeds() {
-        this.embeds = this.embeds.map((e, i) => e.setFooter({
-            text: this.footer(this.index + 1 + i, this.embeds.length)
-        }))
-    }
-    /**
-     * @param {Discord.ReplyMessageOptions | Discord.InteractionReplyOptions} opts 
-     * @description Edits the reply to the message with the given data.
-     * @example
-     * let embed1 = new Discord.MessageEmbed().setDescription('no')
-     * .edit({ embeds : [embed1] })
-     */
+
     edit(opts) {
-        if (this.ended || this.deleted) return;
-        if (this.ctx == "interaction") return this.interaction.editReply(opts).catch(console.error)
-        else if (this.ctx == "message") return this.reply.edit(opts).catch(console.error)
-        else return this.e('No message to edit.')
+        if (this.ended || this.deleted) return Promise.reject('No message to edit.')
+        if (this.ctx == "INTERACTION") return this.interaction.editReply(opts)
+        else if (this.ctx == "MESSAGE") return this.reply.edit(opts)
     }
-    /**
-     * @description Will disable all the buttons.
-     * @example
-     * .disableButtons()
-     */
+
     disableButtons() {
-        if (!this.ctx) return this.e("No reply sended to disable it's embeds?")
-        return this.edit({ components: [this.getRow(true)] }).catch(console.error)
+        return this.edit({ components: [this.getRow(true)] })
     }
-    /**
-     * @description Ends the embed page system.
-     * @returns {void}
-     */
+
     end() {
-        if (!this.ctx) return this.e("Nothing to end.")
-        if (this.ended) return this.e('EBPS already ended.');
-        this.disableButtons()
-        return this.ended = true;
-    }
-    /**
-     * @description Deletes the remply to the embed.
-     */
-    delete() {
-        if (!this.ctx) return this.e('No message/interaction to delete ?');
-        if (this.deleted) return;
-        this.deleted = true
+        if(this.ended) return Promise.resolve();
         this.ended = true;
-        if (this.ctx == "message") return this.reply.delete();
-        else return this.interaction.deleteReply();
+        return this.disableButtons()
     }
-    /**
-     * @returns {Discord.MessageEmbed}
-     * @description Gives the current showed embed.
-     * @example
-     * .embed() instanceof Discord.MessageEmbed // true
-     */
+
+    delete() {
+        this.deleted = true;
+        this.ended = true;
+        if (this.ctx == "MESSAGE") return this.reply.delete();
+        else if (this.ctx == "INTERACTION") return this.interaction.deleteReply();
+        else return Promise.reject('No message to edit.');
+    }
+
     embed() {
         return this.embeds[this.index]
     }
-    /**
-     * @description Refresh the embed.
-     * @returns {Promise<Discord.Message>} The edited reply.
-     */
+
     update() {
         return this.edit(this.getOpts())
     }
-    /**
-     * 
-     * @param {boolean} disabled If the buttons should be returned disabled 
-     * @returns {Discord.MessageOptions} Options used when editing/sending the page system embeds with buttons
-     */
-    getOpts(disabled) {
+
+    getOpts(disabled = false) {
         return {
             embeds: [this.embed()],
             components: [this.getRow(disabled)]
         };
     }
+    isMessage() {
+        return this.ctx ? this.ctx === "MESSAGE" : null;
+    }
+    isInteraction() {
+        return this.ctx ? this.ctx === "INTERACTION" : null;
+    }
+    canEdit() {
+        return this.ctx && !this.ended && !this.deleted
+    }
+    isValidCtx(ctx) {
+        return [CommandInteraction, Message].some(c => ctx instanceof c);
+    }
+    checkForErrors(msg) {
+        let errors = []
+        function add(error) { this.errors.push(error) };
+        if (!this.isValidCtx(msg)) add('Invalid context. Must be a message or interaction');
+        if (!this.ctx) add('No embed sent yet.')
+        if (this.ended) add('Page system has ended already.')
+        if (!msg) add('No message/interaction given.');
+        if (msg instanceof Message && msg.author) {
+            this.message = msg;
+            this.setUserID(msg.author.id)
+        } else if (msg instanceof CommandInteraction && msg.user) {
+            this.interaction = msg;
+            this.setUserID(msg.user.id)
+        } else if (msg instanceof Interaction) {
+            add(`${msg} is an interaction but not a command (it's type is "${msg.type}").`)
+        } else add(`${msg} is neiher an interaction or a message.`);
 
-    /**
-     * @param {Discord.Message | Discord.CommandInteraction} ctx
-     * @description Starts the embed pages. Make sure you use this method after you've added the embeds, set the channel, added the ids etc..
-     * @returns {Discord.Message} The reply to the message if you want to use it
-     */
+        if (!this.embeds.length) add('Embed array is empty.')
+        else {
+            this.embeds.forEach((embed, i) => {
+                if (!(embed instanceof MessageEmbed)) add(`Embed at position ${i} is not an embed.`);
+            })
+        }
+        if (typeof (this.index) !== "number"
+            || isNaN(this.index)
+            || this.index < 0
+            || this.index > 10) add(`Invalid index ${this.index}`);
+
+        if (!this.embeds[this.index]) add(`No embed at index ${this.index}`);
+
+        if (typeof (this.footer) !== "function")
+            add(`Footer is not a function but a ${typeof (this.footer)}.`)
+
+        if (typeof (this.filter) !== "function")
+            add(`Filter is not a function but a ${typeof (this.footer)}.`)
+
+        if (typeof (this.id) !== "string") add(`ID "${this.id}" is not a string.`)
+        if (!(this.id.split(' ').every(c => Number(c)))) add(`ID "${this.id}" is not a valid user ID.`);
+        return errors;
+    }
+
     async start(ctx) {
-        if (!ctx) return this.e('No message/interaction given.');
-        if (ctx instanceof Discord.Message || ctx.author) {
-            this.message = ctx;
-            this.ctx = "message";
-            this.setUserID(ctx.author.id)
-        } else if (ctx instanceof Discord.CommandInteraction || ctx.user) {
-            this.interaction = ctx;
-            this.ctx = "interaction";
-            this.setUserID(ctx.user.id)
-        } else if (ctx instanceof Discord.Interaction){
-            return this.e(`${ctx} is an interaction but not a command (it's type is "${ctx.type}").`)
-        } else return this.e(`${ctx} is neiher an interaction or a message.`);
+        let errors = this.checkForErrors(ctx)
+        if (errors.length)
+            throw new Error("Errors ecountered :\n" + errors.map((e, i) => `${i + 1} - ${e}`).join('\n'))
 
-        let opts = this.getOpts();
-
-        this.reply = await this[this.ctx].reply({ fetchReply: true, ...opts });
-
-        if (!this.reply) return this.e('Could not fetch reply of the message/interaction');
-        if (!this.getRow()) return;
-        this.collector = this.reply.createMessageComponentCollector({
-            componentType: 'BUTTON',
-            time: this.duration
+        this.reply = await this[this.type].reply({
+            fetchReply: true,
+            ...this.getOpts()
         });
 
-        this.collector.on('collect', int => {
-            if (!int.isButton()) return;
-            if (!this.filter(int)) return;
-            int.deferUpdate();
+        if (!this.reply) throw new Error('Could not fetch reply of the message/interaction');
+        if (!this.getRow()) return;
+
+        this.collector = this.reply.createMessageComponentCollector({
+            componentType: 'BUTTON',
+            time: this.duration,
+        });
+
+        this.collector.on('collect', (int) => {
+            if(!this.filter(int)) return;
             switch (int.customId) {
                 case 'next':
                     this.next();
@@ -360,11 +254,13 @@ module.exports.PageSystem = class PageSystem {
                     this.previous();
                     break;
                 case 'stop':
-                    return this.collector.emit('end');
+                    this.collector.emit('end');
             }
             if (!this.ended) this.update();
         });
-        this.collector.on('end', () => !this.ended ? this.end() : true)
+
+        this.collector.on('end', () =>{ this.end() });
+
         return this.reply;
     }
 }
