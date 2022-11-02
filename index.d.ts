1764501b7ca3d650a1f20f918cc21a1e4b508f2a
voxlinou1 <ptdrrrrr.t.ki@gmail.com>
2022-11-02 04:16:32 +0100

    v14

--- index.d.ts
+++ index.d.ts
@@ -1,81 +1,88 @@
-export = Dispage;
 import {
     Client,
-    MessageEmbed,
-    Message,
-    MessageButtonStyle,
     InteractionCollector,
-    UserResolvable,
-    MessageOptions,
-    MessageEditOptions,
-    MessageActionRow,
+    Message,
+    ActionRowBuilder,
     Interaction,
-    ButtonInteraction,
-    MessageButtonOptions
-} from 'discord.js';
+    ButtonStyle,
+    BaseMessageOptions,
+    EmbedBuilder,
+    UserResolvable,
+    ButtonInteraction
+} from "discord.js";
 
 // Types
 type ButtonId = "previous" | "stop" | "next" | string
-type Embed = MessageEmbed
+type Embed = EmbedBuilder
 type User = UserResolvable
 type Context = Message | Interaction
 
 declare class Dispage {
+    /** @param {Client} client  */
     constructor(client: Client);
-    client: Client;
+    client: Client<boolean>;
+    stopReason: string;
     index: number;
+    /** @type {Embed[]} */
     embeds: Embed[];
-    message: Message | null;
-    interaction: Interaction | null;
+    message?: Message;
+    interaction?: Interaction;
     collector: InteractionCollector<ButtonInteraction>;
     reply: Message;
     ended: boolean;
     started: boolean;
     deleted: boolean;
     duration: number;
-    mainStyle: MessageButtonStyle;
-    buttons: MessageButtonOptions[];
+    mainStyle: ButtonStyle;
+    styles: string[];
+    buttons: {
+        customId: string;
+        emoji: string;
+        style: string;
+        label: string;
+    }[];
     filter: (int: ButtonInteraction) => boolean;
-    _sdb: boolean;
+    _hdb: boolean;
+    /** @type {Set<string>} */
     _userIDs: Set<string>;
     get users(): string[];
     get ctx(): "MESSAGE" | "INTERACTION";
     get type(): string;
     get endUntill(): number;
+    /** @returns {Embed} */
     get currentEmbed(): Embed;
-    _getId(user: User): string;
-    setMainStyle(style: MessageButtonStyle): this;
-    showDisabledButtons(should?: boolean): this;
+    _getId(user: any): any;
+    setMainStyle(style: ButtonStyle): this;
+    hideDisabledButtons(should?: boolean): this;
     removeUser(user: User): this;
     addUser(user: User): this;
     setUser(user: User): this;
-    addButton(o: MessageButtonOptions): this;
+    addButton(o: object): this;
     removeAllButtons(): this;
     findButtonAndRemove(query: Function): this;
-    removeButton(customId: ButtonId): this;
-    editButton(customId: ButtonId, o: MessageButtonOptions): this;
-    getRows(disabled?: boolean): MessageActionRow | null;
+    removeButton(customId: string): this;
+    editButton(customId: string, o: object): this;
+    getRows(disabled?: boolean): ActionRowBuilder | null;
     setEmbeds(embeds: Embed[]): this;
     addEmbed(embed: Embed): this;
     addEmbeds(embeds: Embed[]): this;
     _fixEmbeds(embeds: Embed | Embed[] | Embed[][]): Embed[];
     setDuration(duration: number): this;
     addDuration(duration: number): this;
-    next(): Promise<Message | false>;
-    previous(): Promise<Message | false>;
+    next(int: any): Promise<Message | false>;
+    previous(int: any): Promise<Message | false>;
     doesIndexExist(index: number): boolean;
-    changeToPage(index: number): Promise<Message>;
+    changeToPage(index: number, int: any): Promise<Message>;
     setIndex(index: number): this;
-    edit(opts: MessageEditOptions): Promise<Message>;
-    end(reason: "button" | "time"): Promise<void | Message>;
+    end(reason: string): Promise<void | Message>;
     delete(): Promise<Message>;
-    update(): Promise<Message>;
-    getOpts(disabled?: boolean): MessageOptions;
+    getOpts(disabled?: boolean): BaseMessageOptions;
     isMessage(): boolean;
     isInteraction(): boolean;
     canEdit(): boolean;
     isValidCtx(ctx: Context): boolean;
-    _createCollector(reply: Message): InteractionCollector<ButtonInteraction>;
-    checkForErrors(ctx: Context): string[];
+    _createCollector(reply: Message): InteractionCollector<any>;
+    checkForErrors(ctx: any): string[];
     start(ctx: Context): Promise<this>;
 }
+export = Dispage;
